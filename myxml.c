2dde9f66cb117cc4d67610fabcb31be5291efd22
jiming.cai <623516556@qq.com>
2021-06-03 11:45:15 +0800

    add xml config

---
+++ myxml.c
@@ -0,0 +1,15 @@
+#include "common.h"
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+//refer http://blog.chinaunix.net/uid-25695950-id-3921541.html
+
+void xml_test() {
+    xmlDocPtr doc=NULL;
+    xmlNodePtr cur=NULL;
+    char* name=NULL;
+    char* value=NULL;
+
+    xmlKeepBlanksDefault (0);
+
+
+}
